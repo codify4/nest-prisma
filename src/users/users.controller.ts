b30b78d5809bb2ed01d48dbb236e6155b3c94ab0
IjonK007 <kushta.joni@gmail.com>
2024-09-03 13:51:54 +0200

    added basic get and post routes for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Param, ParseIntPipe, Post, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    createUser(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+        return this.usersService.createUser(createUserDto);
+    }
+
+    @Get()
+    getUsers() {
+        return this.usersService.getUsers();
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id', ParseIntPipe) id: number) {
+        const user = await this.usersService.getUserById(id);
+        if(!user) throw new HttpException('User not found', 404);
+        return user;
+    }
+}
