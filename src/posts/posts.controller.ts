6e531879495f4f82e6c14e2289b9a9237e351717
codify4 <kushta.joni@gmail.com>
2024-09-05 08:26:29 +0200

    auth kinda finished, posts error

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { PostsService } from './posts.service';
 import { CreateGroupPostDto } from './dtos/create-group-post.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('posts')
 export class PostsController {
@@ -12,11 +13,13 @@ export class PostsController {
         return this.postsService.createPost(userId, createPostDto);
     }
     
+    @UseGuards(JwtAuthGuard)
     @Post('group')
     createGroupPost(@Body(ValidationPipe) {userIds, ...createGroupPostDto}: CreateGroupPostDto) {
         return this.postsService.createGroupPost(userIds, createGroupPostDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('group')
     getGroupPosts() {
         return this.postsService.getGroupPosts();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, HttpException, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, Param, ParseIntPipe, Patch, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UpdateUserSettingsDto } from './dtos/update-settings.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
@@ -13,11 +14,13 @@ export class UsersController {
         return this.usersService.createUser(createUserDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     getUsers() {
         return this.usersService.getUsers();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
     async getUserById(@Param('id', ParseIntPipe) id: number) {
         const user = await this.usersService.getUserById(id);
@@ -25,16 +28,19 @@ export class UsersController {
         return user;
     }
 
+    @UseGuards(JwtAuthGuard)
     @Patch(':id')
     updateUserById(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
         return this.usersService.updateUserById(id, updateUserDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     deleteUserById(@Param('id', ParseIntPipe) id: number) {
         return this.usersService.deleteUserById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Patch(':id/settings')
     updateUserSettingsById(
         @Param('id', ParseIntPipe) id: number, 
